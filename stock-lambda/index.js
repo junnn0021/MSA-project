dc549d372fa90f842f5fc876777bc7e74fb9198e
Park-Seaweed <qkralsgur721@gmail.com>
2023-03-04 16:12:17 +0900

    add/stock-lambda: 제품 만드는 공장으로 payload담아서 POST요청 보내기

--- stock-lambda/index.js
+++ stock-lambda/index.js
@@ -1,8 +1,24 @@
 const axios = require('axios').default
 exports.handler = async (event) => {
-  const a = JSON.parse(event.Records[0].body).MessageAttributes
-  console.log(a)
+  const { MessageId, MessageAttributeProductId, MessageAttributeFactoryId } = JSON.parse(event.Records[0].body).MessageAttributes
+  console.log(MessageAttributeFactoryId, MessageAttributeProductId)
 
+  const payload = {
+    MessageGroupId: MessageId,
+    MessageAttributeProductId: MessageAttributeProductId.Value,
+    MessageAttributeProductCnt: "10",
+    MessageAttributeFactoryId: MessageAttributeFactoryId.Value,
+
+  }
+  console.log(payload)
+
+  axios.post('http://project3-factory-api.coz-devops.click/api/manufactures', payload)
+    .then(function (response) {
+      console.log(response);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
 
 
 }
